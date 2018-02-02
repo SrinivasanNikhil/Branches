625f157fe7b855361a29452556ce878bbe3b2cf6
SrinivasanNikhil <nikhil.srinivasan@gmail.com>
2018-02-02 08:50:44 -0500

    Initial commit

---
+++ src/Conditionals.java
@@ -0,0 +1,29 @@
+
+public class Conditionals {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int a = 10;
+		int b = 10;
+		
+		if(a > b)
+		{
+			System.out.println("A is greater");
+		}
+		else if (b > a)
+		{
+			System.out.println("B is greater");
+		}
+		else
+		{
+			System.out.println("They may be equal");
+		}
+		
+		
+		
+		
+	}
+
+}
